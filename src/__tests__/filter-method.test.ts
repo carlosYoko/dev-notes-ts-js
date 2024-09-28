851ea6820cc821804bf9447c098d5d33418b6c4e
Carlos Gimenez Berral <carlos.gimenez@external.wolterskluwer.com>
2024-09-28 16:52:49 +0200

    mock test filter

--- src/__tests__/filter-method.test.ts
+++ src/__tests__/filter-method.test.ts
@@ -1,24 +1,44 @@
 import { IUser, User, filteredYear } from "../filter-method";
 
-test("filtered less than year 86's", () => {
+describe('testing ', () => {
 
-    // Act
-    let users: IUser[] = [new User("Test1", 94), new User("Test2", 86), new User("Test3", 76)]
-    let expected: IUser[] = [{ Name: 'Test2', Year: 86 }, { Name: 'Test3', Year: 76 }]
+    test("filtered less than year 86's", () => {
 
-    // Arrange
-    let result: IUser[] = users.filter(filteredYear);
+        // Act
+        const mockUsers: IUser[] = [{ Name: 'Test1', Year: 94 }, { Name: 'Test2', Year: 86 }, { Name: 'Test3', Year: 76 }];
+        const expected: IUser[] = [{ Name: 'Test2', Year: 86 }, { Name: 'Test3', Year: 76 }];
 
-    // Assert
-    expect(result).toEqual(expected);
-    expect(result.length).toBe(2);
-    expect(result).toHaveLength(2);
-    expect(result).toContainEqual(expected[0]);
-    expect(result).toContainEqual(expected[1]);
-    expect(result).toEqual(expect.arrayContaining(result));
+        const mockFilter = jest.fn(filteredYear);
+        const result = mockUsers.filter(mockFilter);
 
-    result.forEach(user => {
-        expect(user).not.toBeNull();
+        // Assert
+        expect(result).toEqual(expected);
+        expect(result).toHaveLength(2);
+        expect(result).toContainEqual(expected[0]);
+        expect(result).toContainEqual(expected[1]);
+        expect(mockFilter).toHaveBeenCalledTimes(mockUsers.length);
     });
 
-});
+    test("filtered less than year 86's with fakedata", () => {
+
+        // Act
+        let users: IUser[] = [new User("Test1", 94), new User("Test2", 86), new User("Test3", 76)]
+        let expected: IUser[] = [{ Name: 'Test2', Year: 86 }, { Name: 'Test3', Year: 76 }]
+
+        // Arrange
+        let result: IUser[] = users.filter(filteredYear);
+
+        // Assert
+        expect(result).toEqual(expected);
+        expect(result.length).toBe(2);
+        expect(result).toHaveLength(2);
+        expect(result).toContainEqual(expected[0]);
+        expect(result).toContainEqual(expected[1]);
+        expect(result).toEqual(expect.arrayContaining(result));
+
+        result.forEach(user => {
+            expect(user).not.toBeNull();
+        });
+
+    });
+})
